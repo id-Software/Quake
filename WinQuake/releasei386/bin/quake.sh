b2951690415054ae8705c07f8df7a4ba7cde382a
wyatt8740 <wyatt8740@yahoo.com>
2014-01-06 11:55:31 -0500

    Updated for modern linux i386 systems - fixed makefiles, added X11 and GLX makefiles and README

---
+++ WinQuake/releasei386/bin/quake.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+#Change the following to fit your screen resolution and memory requirements
+aoss ./glquake.glx -width 1280 -height 1024 -mem 64 -fullscreen
+#Quake turns key repetition off. Quick temp fix here
+xset r on
